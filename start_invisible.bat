9b2a9452af6b5a612f6da22d7aaa0d1a75bf96da
Vincy-2515 <110780936+Vincy-2515@users.noreply.github.com>
2025-03-15 14:56:54 +0100

    generato eseguibile

--- start_invisible.bat
+++ start_invisible.bat
@@ -1,4 +1,4 @@
-echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\run_invisible.vbs" & rem Scrive il comando per creare l'oggetto WshShell nel file VBS
-echo WshShell.Run "cmd /c Parrot_BOT.exe > Parrot_BOT.log 2>&1", 0, False >> "%temp%\run_invisible.vbs" & rem Scrive il comando per eseguire il programma e reindirizzare l'output al file di log
-cscript //nologo "%temp%\run_invisible.vbs" & rem Esegue il file VBS senza mostrare la finestra della console
-del "%temp%\run_invisible.vbs" & rem Rimuove il file VBS temporaneo dopo l'esecuzione
+echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\run_invisible.vbs"
+echo WshShell.Run "cmd /c Parrot_BOT.exe > Parrot_BOT.log 2>&1", 0, False >> "%temp%\run_invisible.vbs"
+cscript //nologo "%temp%\run_invisible.vbs"
+del "%temp%\run_invisible.vbs"
