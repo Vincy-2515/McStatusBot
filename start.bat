12801de630392828882342a7958a7856007a18e6
Vincy-2515 <110780936+Vincy-2515@users.noreply.github.com>
2025-06-13 00:07:34 +0200

    rigenerato eseguibile

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-start Reforged_BOT.bat noconsole
+start Reforged_BOT.bat
 
 ::powershell Get-Process -Name Reforged_BOT -ErrorAction Ignore >nul 2>&1 && ECHO Warning! The bot is still running.
